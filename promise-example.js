aa4dc3055077e9080e84c414e69cb1ff2d366c01
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2016-06-10 15:11:50 +0200

    Create promise-example.js

---
+++ promise-example.js
@@ -0,0 +1,25 @@
+function asyncFn(){
+      var success = function(c){};
+      var error = function(c){};
+      //Promise
+
+      setTimeout(function(){
+        success('async finished!');
+      },2000);
+
+      //Promise
+        return {
+            then: function(cb) {
+                success = cb;
+                return this;
+            },
+            error: function(cb) {
+                error = cb;
+                return this;
+            }
+        };
+    }
+    
+    asyncFn().then(function(){
+      console.log('I\'m done!');
+    });
